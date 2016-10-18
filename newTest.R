9a6a6bea5c9a296c4086cec1ebb9dca7a99b2c25
Penn <cpenn@usgs.gov>
2016-10-18 11:28:06 -0600

    test

---
+++ newTest.R
@@ -0,0 +1,6 @@
+staid <- c("09163500")
+library(lubridate)
+qwdata <- readNWISodbc(DSN = "NWISCO", STAIDS = staid)
+# library(WQReview)
+# qwdata <- WQReview::readNWISodbc(DSN = "NWISCO", STAIDS = staid)
+x <- qwdata$PlotTable
