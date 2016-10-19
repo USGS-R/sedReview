46899dc1808d931b4abd6cb2241d128554169336
Penn <cpenn@usgs.gov>
2016-10-19 15:41:50 -0600

    sedOutliers updates

--- R/sedOutliers.R
+++ R/sedOutliers.R
@@ -6,17 +6,23 @@ sedOutliers <- function(x){
   bedLOI <- x[x$PARM_CD == 00496, ]
   watLOI <- x[x$PARM_CD == 00535, ]
   
-  # closure function for printing plot and percentiles
+  # quantiles
+  quantiles <- data.frame(quantile = seq(0, 1, 0.1))
+  quantiles$SSC <- quantile(SSC$RESULT_VA, probs = seq(0, 1, 0.1))
+  quantiles$SSL <- quantile(SSL$RESULT_VA, probs = seq(0, 1, 0.1))
+  quantiles$bedLOI <- quantile(bedLOI$RESULT_VA, probs = seq(0, 1, 0.1))
+  quantiles$watLOI <- quantile(watLOI$RESULT_VA, probs = seq(0, 1, 0.1))
+  
+  
+  # closure function for printing plot
   f <- function(d) {
     if(nrow(d) > 0){
       title <- paste(unique(d$PARM_CD), unique(d$PARM_NM), sep = " ")
-      bp <- boxplot(d$RESULT_VA)
+      boxplot(d$RESULT_VA)
       title(title)
-      print(title)
-      quantile(d$RESULT_VA, probs = seq(0, 1, 0.1), na.rm = TRUE)
-    } else{
-      print(paste("No Samples"))
-    }
+    } else{}
   }
+  lapply(list(SSC, SSL, bedLOI, watLOI), f)
+  return(quantiles)
 }
 
--- R/testFunctions.R
+++ R/testFunctions.R
@@ -1,7 +1,11 @@
 # pull station data to test
 staid <- c("09163500")
 library(lubridate)
+source("R/converTime.R")
+source("R/readNWISodbc.R")
 qwdata <- readNWISodbc(DSN = "NWISCO", STAIDS = staid)
 x <- qwdata$PlotTable
 
 # for testing function calls
+source("R/sedOutliers.R")
+sedOutliers(x)
