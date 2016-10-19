e4dd4a8a010dfae659276f528a66d65d4aad717b
Penn <cpenn@usgs.gov>
2016-10-19 16:19:49 -0600

    Sand/Silt break check sample purpose codes

--- R/testFunctions.R
+++ R/testFunctions.R
@@ -1,6 +1,7 @@
 # pull station data to test
 staid <- c("09163500")
 library(lubridate)
+library(dplyr)
 source("R/converTime.R")
 source("R/readNWISodbc.R")
 qwdata <- readNWISodbc(DSN = "NWISCO", STAIDS = staid)
@@ -9,3 +10,7 @@ x <- qwdata$PlotTable
 # for testing function calls
 source("R/sedOutliers.R")
 sedOutliers(x)
+source("R/sandSiltBreak.R")
+sandSiltBreak(x)
+source("R/checkSamPurp.R")
+checkSamPurp(x)
