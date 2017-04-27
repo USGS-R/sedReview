38ea56d96bbb3822a20aaf4baf37f2b2c6babba7
tmills-usgs <tmills@usgs.gov>
2017-04-27 11:21:35 -0600

    pulled out wide dataframe from getLocalNWIS and added makeWideTable function to convert format

--- R/getLocalNWIS.R
+++ R/getLocalNWIS.R
@@ -644,37 +644,7 @@ getLocalNWIS <- function(DSN,
   ###Remove reviewed and rejected results
   longTable <- longTable[!(longTable$DQI_CD %in% c("Q","X")),]
   
-  ###Get wide format table
   
-  wideTable <- longTable[!(duplicated(longTable[c("RECORD_NO","PARM_CD")])),]
-  wideTable <- reshape2::dcast(wideTable, RECORD_NO ~ PARM_CD,value.var = "Val_qual")
-  
-  #rename pcodes to parm names
-  parmNames <- as.data.frame(names(wideTable),stringsAsFactors=FALSE)
-  names(parmNames) <- "PARM_CD"
-  parmNames <- dplyr::left_join(parmNames,unique(longTable[c("PARM_CD","PARM_NM")]),by="PARM_CD")
-  parmNames$PARM_NM <- make.unique(make.names(parmNames$PARM_NM))
-  names(wideTable) <- c("RECORD_NO", parmNames$PARM_NM[2:length(parmNames$PARM_NM)])
-  
-  #fill in record number meta data (station ID, name, date, time, etc)
-  metaData <- unique(longTable[c("RECORD_NO","SITE_NO","STATION_NM","SAMPLE_START_DT","SAMPLE_END_DT","MEDIUM_CD",
-                                 "SAMP_TYPE_CD","SITE_TP_CD","AGENCY_CD","PROJECT_CD","AQFR_CD","LAB_NO","HYD_EVENT_CD",
-                                 "SAMPLE_CR","SAMPLE_CN","SAMPLE_MD","SAMPLE_MN",
-                                 "SAMPLE_START_SG","SAMPLE_START_TZ_CD","SAMPLE_START_LOCAL_TM_FG",
-                                 "SAMPLE_END_SG","SAMPLE_END_TZ_CD","SAMPLE_END_LOCAL_TM_FG","SAMPLE_ID",
-                                 "TM_DATUM_RLBLTY_CD","ANL_STAT_CD","HYD_COND_CD",
-                                 "TU_ID","BODY_PART_ID","COLL_ENT_CD","SIDNO_PARTY_CD")])
-  wideTable <- dplyr::left_join(wideTable,metaData,
-                           by="RECORD_NO")
-  
-
-  #reorder columns so meta data is at front
-  parmcols <- seq(from =2, to =ncol(wideTable)-ncol(metaData)+1)
-  metacols <- seq(from = ncol(wideTable)-(ncol(metaData)-2), to =ncol(wideTable))
-  wideTable <- wideTable[c(1,metacols[1:8],parmcols,metacols[9:length(metacols)])]
-  
-  
-  return(list(longTable=longTable,
-              wideTable=wideTable))
+  return(longTable)
   
 }
