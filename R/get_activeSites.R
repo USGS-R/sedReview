2c23c45397feb89172ae7148b35c28834c16f75e
tmills-usgs <tmills@usgs.gov>
2017-06-02 11:54:27 -0600

    get_activeSites start

--- R/get_activeSites.R
+++ R/get_activeSites.R
@@ -7,20 +7,20 @@
 #activeSTAIDS <- get_activeSites(DSN="NWISCO",env.db="01)
 #sedData <- get_localNWIS(STAIDS = activeSTAIDS)
 
-DSN <- "NWISCO"
-env.db = "01"
-
-
-Query <- paste("select * from ", DSN, ".QW_SAMPLE_",env.db,sep="")
-Samples <- RODBC::sqlQuery(Chan1, Query, as.is=T)
-
-#Need to subset by date in the SQL querry
-Query <- paste0("select * from ", DSN, ".QW_SAMPLE_",env.db," where sample_start_dt between '",
-                format(Sys.Date() - 365*5,"%Y-%m-%d %H:%M:%S"),
-                "' and '",
-                format(Sys.Date()+1,"%Y-%m-%d %H:%M:%S"),"'")
-Samples <- RODBC::sqlQuery(Chan1, Query, as.is=T)
-
-
-Query <- paste0("select * from NWISCO.QW_SAMPLE_01 where sample_start_dt between cast('2010-01-01' as datetime) and cast('2015-01-01' as datetime)")
-
+# DSN <- "NWISCO"
+# env.db = "01"
+# 
+# 
+# Query <- paste("select * from ", DSN, ".QW_SAMPLE_",env.db,sep="")
+# Samples <- RODBC::sqlQuery(Chan1, Query, as.is=T)
+# 
+# #Need to subset by date in the SQL querry
+# Query <- paste0("select * from ", DSN, ".QW_SAMPLE_",env.db," where sample_start_dt between '",
+#                 format(Sys.Date() - 365*5,"%Y-%m-%d %H:%M:%S"),
+#                 "' and '",
+#                 format(Sys.Date()+1,"%Y-%m-%d %H:%M:%S"),"'")
+# Samples <- RODBC::sqlQuery(Chan1, Query, as.is=T)
+# 
+# 
+# Query <- paste0("select * from NWISCO.QW_SAMPLE_01 where sample_start_dt between cast('2010-01-01' as datetime) and cast('2015-01-01' as datetime)")
+# 
