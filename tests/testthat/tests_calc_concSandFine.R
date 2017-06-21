bbc2f3926ede9ea22bb253907a90454a3e01d62a
cpenn-usgs <cpenn@usgs.gov>
2017-06-21 15:26:29 -0600

    CI test for calc_concSandFine

---
+++ tests/testthat/tests_calc_concSandFine.R
@@ -0,0 +1,12 @@
+context("calc_concSandFiine")
+
+test_that("Return values check", {
+  data("exampleData", package = "sedReview")
+  
+  #calc_concSandFineOut <- calc_concSandFine(exampleData)
+  #saveRDS(calc_concSandFineOut, "tests/testthat/data/calc_concSandFineOut.rds")
+  
+  calc_concSandFineOut <- readRDS("data/calc_concSandFineOut.rds")
+  calc_concSandFineOutTest <- calc_concSandFine(exampleData)
+  expect_equal(calc_concSandFineOut, calc_concSandFineOutTest)
+})
