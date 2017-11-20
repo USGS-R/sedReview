df0de72713537d50eb3e762326098a109883f7a9
Colin Penn <cpenn@usgs.gov>
2017-11-20 11:39:03 -0700

    fix CI tests

--- tests/testthat/tests_check_samplerType.R
+++ tests/testthat/tests_check_samplerType.R
@@ -2,6 +2,10 @@ context("check_samplerType")
 
 test_that("Return values check", {
   data("exampleData", package = "sedReview")
+  
+  #check_samplerTypeOut <- check_samplerType(exampleData)
+  #saveRDS(check_samplerTypeOut,"tests/testthat/data/check_samplerTypeOut.rds")
+  
   check_samplerTypeOut <- readRDS("data/check_samplerTypeOut.rds")
   check_samplerTypeOutTest <- check_samplerType(exampleData)
   expect_equal(check_samplerTypeOut, check_samplerTypeOutTest)
--- tests/testthat/tests_find_outliers.R
+++ tests/testthat/tests_find_outliers.R
@@ -4,7 +4,7 @@ test_that("Return values check", {
   data("exampleData", package = "sedReview")
   
   #find_outliersOut <- find_outliers(exampleData)
-  #saveRDS(find_outliersOut,"data/find_outliersOut.rds")
+  #saveRDS(find_outliersOut,"tests/testthat/data/find_outliersOut.rds")
   
   find_outliersOut <- readRDS("data/find_outliersOut.rds")
   find_outliersOutTest <- find_outliers(exampleData)
