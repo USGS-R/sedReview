d69638044d01c5b126de59fbcf2ee32bfa437d29
Colin Penn <cpenn@usgs.gov>
2017-11-21 16:13:22 -0700

    more find_provisional

--- R/find_provisional.R
+++ R/find_provisional.R
@@ -1,11 +1,14 @@
 
+data('exampleData', package = "sedReview")
+x <- exampleData
+
 env.db = "01"
 qa.db = "02"
 
-# get samples with DQI of S
-x <- x[x$DQI_CD == 'S',]
+# get sediment samples with DQI of S
+x <- x[x$DQI_CD == 'S' & x$PARM_CD %in% c("NEED LIST OF PCODES FROM MOLLY"),]
 
-#split records into env and qa database
+# convert db numbers to character if necessary
 if(is.numeric(env.db)){
   env.db <- formatC(env.db, width = 2, format = "d", flag = "0")
 }
@@ -13,8 +16,10 @@ if(is.numeric(qa.db)){
   qa.db <- formatC(qa.db, width = 2, format = "d", flag = "0")
 }
 
+#split records into env and qa database
 env.recs <- grep(pattern = paste0("_",env.db), x$RECORD_NO, value = TRUE, fixed = TRUE)
 env.x <- x[x$RECORD_NO %in% env.recs,]
 
 qa.recs <- grep(pattern = paste0("_",qa.db), x$RECORD_NO, value = TRUE, fixed = TRUE)
 qa.x <- x[x$RECORD_NO %in% qa.recs,]
+
