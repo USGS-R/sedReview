3753d0c271af08ab8d39b93d5fcc248d97226ce2
Colin Penn <cpenn@usgs.gov>
2017-11-22 09:40:48 -0700

    more find_provisional work

--- R/find_provisional.R
+++ R/find_provisional.R
@@ -4,9 +4,17 @@ x <- exampleData
 
 env.db = "01"
 qa.db = "02"
+pcodes = c("80154",
+           "70331",
+           "00530",
+           "80155",
+           "80225",
+           "91145")
+env.fileout = NULL
+qa.fileout = NULL
 
 # get sediment samples with DQI of S
-x <- x[x$DQI_CD == 'S' & x$PARM_CD %in% c("NEED LIST OF PCODES FROM MOLLY"),]
+x <- x[x$DQI_CD == 'S' & x$PARM_CD %in% pcodes,]
 
 # convert db numbers to character if necessary
 if(is.numeric(env.db)){
@@ -22,4 +30,23 @@ env.x <- x[x$RECORD_NO %in% env.recs,]
 
 qa.recs <- grep(pattern = paste0("_",qa.db), x$RECORD_NO, value = TRUE, fixed = TRUE)
 qa.x <- x[x$RECORD_NO %in% qa.recs,]
+
+#viewable table of results
+env.x <- env.x[c("RECORD_NO","SITE_NO","STATION_NM","SAMPLE_START_DT",
+                 "MEDIUM_CD","PARM_CD","PARM_NM","REMARK_CD","RESULT_VA","DQI_CD","SAMPLE_CM_TX")]
+View(env.x)
+
+qa.x <- qa.x[c("RECORD_NO","SITE_NO","STATION_NM","SAMPLE_START_DT",
+               "MEDIUM_CD","PARM_CD","PARM_NM","REMARK_CD","RESULT_VA","DQI_CD","SAMPLE_CM_TX")]
+View(qa.x)
+
+#output record number files if filenames specified. ****NEED TO TEST THAT FILE FORMAT WORKS IN QWDATA****
+if(!is.null(env.fileout)){
+  env.x$RECORD_NO <- sub(pattern = paste0("_",env.db), replacement = "", env.x$RECORD_NO)
+  write.table(env.x, file = env.fileout, quote = FALSE, sep = "\t", row.names = FALSE, col.names = FALSE)
+}
+if(!is.null(qa.fileout)){
+  qa.x$RECORD_NO <- sub(pattern = paste0("_",qa.db), replacement = "", qa.x$RECORD_NO)
+  write.table(qa.x, file = qa.fileout, quote = FALSE, sep = "\t", row.names = FALSE, col.names = FALSE)
+}
 
