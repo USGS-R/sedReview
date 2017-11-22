5c55bd95533aab3a01d5af2e422c0aae39e67654
Colin Penn <cpenn@usgs.gov>
2017-11-22 11:13:25 -0700

    fix find_provisional testthat

--- tests/testthat/tests_find_provisional.R
+++ tests/testthat/tests_find_provisional.R
@@ -3,10 +3,10 @@ context(find_provisional)
 test_that("Return values check", {
   data("exampleData", package = "sedReview")
   
-  # find_provisionalOut <- find_provisional(exampleData)
+  # find_provisionalOut <- find_provisional(exampleData, view = FALSE)
   # saveRDS(find_provisionalOut, file = "tests/testthat/data/find_provisionalOut.rds")
   
   find_provisionalOut <- readRDS("data/find_provisionalOut.rds")
-  find_provisionalOutTest <- find_provisional(exampleData)
+  find_provisionalOutTest <- find_provisional(exampleData, view = FALSE)
   expect_equal(find_provisionalOut, find_provisionalOutTest)
 })
