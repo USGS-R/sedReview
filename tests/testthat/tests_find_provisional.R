d2d4e12eae18f5505f56f21b5d9d967648f5f71d
Colin Penn <cpenn@usgs.gov>
2017-11-22 10:52:45 -0700

    tests for find_provisional

---
+++ tests/testthat/tests_find_provisional.R
@@ -0,0 +1,12 @@
+context(find_provisional)
+
+test_that("Return values check", {
+  data("exampleData", package = "sedReview")
+  
+  # find_provisionalOut <- find_provisional(exampleData)
+  # saveRDS(find_provisionalOut, file = "tests/testthat/data/find_provisionalOut.rds")
+  
+  find_provisionalOut <- readRDS("data/find_provisionalOut.rds")
+  find_provisionalOutTest <- find_provisional(exampleData)
+  expect_equal(find_provisionalOut, find_provisionalOutTest)
+})
