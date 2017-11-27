0a91d5f7c05902e70db82c84873a0a25862f10a2
Colin Penn <cpenn@usgs.gov>
2017-11-27 12:46:20 -0700

    add summary stats

--- R/calc_summaryStats.R
+++ R/calc_summaryStats.R
@@ -13,6 +13,10 @@ pcodes = c("80154",
 #limit data to pcodes of interest and remove blanks ******ASK MOLLY HOW TO HANDLE REMARKS*****
 x <- x[!(x$MEDIUM_CD == "OAQ") & x$PARM_CD %in% pcodes, ]
 
-#group by site, by WY, by pcode and calc stats
-temp <- dplyr::summarise(dplyr::group_by(x,SITE_NO,WY,PARM_CD),
-                         mean = mean(x$RESULT_VA))
+#group by site, by pcode, by WY and calc stats
+temp <- dplyr::summarise(dplyr::group_by(x,SITE_NO,STATION_NM,PARM_CD,PARM_NM,WY),
+                         min = min(RESULT_VA),
+                         max = max(RESULT_VA),
+                         median = median(RESULT_VA),
+                         mean = mean(RESULT_VA),
+                         stdev = sd(RESULT_VA, na.rm = T))
