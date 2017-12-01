d873e418c67442a69c7384ee601c36eab653e507
Colin Penn <cpenn@usgs.gov>
2017-12-01 15:10:40 -0700

    test summary_boxcoef, tests with exampleData2

--- tests/testthat/tests_find_boxcoef.R
+++ tests/testthat/tests_find_boxcoef.R
@@ -1,12 +1,12 @@
 context("find_boxcoef")
 
 test_that("Return values check", {
-  data("exampleData", package = "sedReview")
+  data("exampleData2", package = "sedReview")
   
-  # find_boxcoefOut <- find_boxcoef(x, site_no = "05586300")
+  # find_boxcoefOut <- find_boxcoef(exampleData2, site_no = "09163500")
   # saveRDS(find_boxcoefOut, "tests/testthat/data/find_boxcoefOut.rds")
   
   find_boxcoefOut <- readRDS("data/find_boxcoefOut.rds")
-  find_boxcoefOutTest <- find_boxcoef(x, site_no = "05586300")
+  find_boxcoefOutTest <- find_boxcoef(exampleData2, site_no = "09163500")
   expect_equal(find_boxcoefOut, find_boxcoefOutTest)
 })
