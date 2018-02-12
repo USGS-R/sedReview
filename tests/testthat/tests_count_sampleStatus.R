4c3006449c4b80102f58185e8dc0f5b7cea450e5
Colin Penn <cpenn@usgs.gov>
2018-02-12 10:27:15 -0700

    fix CI tests

--- tests/testthat/tests_count_sampleStatus.R
+++ tests/testthat/tests_count_sampleStatus.R
@@ -5,7 +5,7 @@ test_that("Return values check", {
   
   # count_sampleStatusOut <- count_sampleStatus(exampleData)
   # saveRDS(count_sampleStatusOut,"tests/testthat/data/count_sampleStatusOut.rds")
-  
+
   count_sampleStatusOut <- readRDS("data/count_sampleStatusOut.rds")
   count_sampleStatusOutTest <- count_sampleStatus(exampleData)
   expect_equal(count_sampleStatusOut, count_sampleStatusOutTest)
