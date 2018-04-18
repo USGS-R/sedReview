934778a358c831157462ae991fb3b0fb4cc539d5
Colin Penn <cpenn@usgs.gov>
2018-04-18 13:39:44 -0600

    add station_nm and parm_nm to sample status table

--- R/count_sampleStatus.R
+++ R/count_sampleStatus.R
@@ -16,21 +16,21 @@
 
 
 count_sampleStatus <- function(x, bySite = TRUE) {
-  x <- x[c("UID","RECORD_NO","SITE_NO","STATION_NM","SAMPLE_START_DT","MEDIUM_CD","PARM_CD","DQI_CD","RESULT_VA")]
+  x <- x[c("UID","RECORD_NO","SITE_NO","STATION_NM","SAMPLE_START_DT","MEDIUM_CD","PARM_CD","PARM_NM","DQI_CD","RESULT_VA")]
   x <- x[x$PARM_CD %in% c('80154','70331','91157','00530','91145','80225'),]
   x <- unique(x)
   
   if(bySite == TRUE){
-  statusSum <- dplyr::summarise(dplyr::group_by(x,SITE_NO,PARM_CD,DQI_CD),
+  statusSum <- dplyr::summarise(dplyr::group_by(x,SITE_NO,STATION_NM,PARM_CD,PARM_NM,DQI_CD),
                                 N = length(RESULT_VA)
   )
-  statusSum <- reshape2::dcast(statusSum,SITE_NO+PARM_CD~DQI_CD,value.var = "N")
+  statusSum <- reshape2::dcast(statusSum,SITE_NO+STATION_NM+PARM_CD+PARM_NM~DQI_CD,value.var = "N")
   
   } else {
-    statusSum <- dplyr::summarise(dplyr::group_by(x,PARM_CD,DQI_CD),
+    statusSum <- dplyr::summarise(dplyr::group_by(x,PARM_CD,PARM_NM,DQI_CD),
                                   N = length(RESULT_VA)
     )
-    statusSum <- reshape2::dcast(statusSum,PARM_CD~DQI_CD,value.var = "N")
+    statusSum <- reshape2::dcast(statusSum,PARM_CD+PARM_NM~DQI_CD,value.var = "N")
   }
   
   statusSum[is.na(statusSum)] <- 0
