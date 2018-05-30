71b3315cf9aa721841e1cb905905a20f79bb067b
Colin Penn <cpenn@usgs.gov>
2018-05-30 16:42:40 -0600

    user-defined POR for summaryStats

--- R/calc_summaryStats.R
+++ R/calc_summaryStats.R
@@ -1,7 +1,10 @@
 #' calc_summaryStats. Calculate summary stats and tally non-detections and samples with no result values reported.
-#' @description Calculates summary statistics, and tallys non-detects and NA result values, grouped by site, parameter, and water year.
+#' @description Calculates summary statistics, and tallys non-detects and NA result values, grouped by site, parameter, 
+#' and water year (or user-defined calendar year period).
 #' @param x A \code{dataframe} output from \code{get_localNWIS}
 #' @param pcodes A character vector of parameter codes of interest. 
+#' @param startMonth Numeric starting month (ex. 1 for January) for user-defined calendar year periods. Default is NULL.
+#' @param endMonth Numeric ending month (ex. 12 for December) for user-defined calendar year periods. Default is NULL. 
 #' Default pcodes are SSC (80154), Sand/silt break on suspended (70331), TSS (00530), SSL (80155), Bedload (80225), Bedload mass (91145)
 #' @details Calculates number of samples, minimum, maximum, median, mean, and standard deviation (if applicable). 
 #' Non-detects (REMARK_CD = "<") and Averages (REMARK_CD = "A") are not included in calculations. Rejected samples are not included.
@@ -9,7 +12,10 @@
 #'  is included in the summary. Summary value "n" does not include non-detects or no result reported samples.
 #' @details Default pcodes are SSC (80154), Sand/silt break on suspended (70331), TSS (00530), SSL (80155), 
 #' Bedload (80225), Bedload mass (91145)
-#' @details Statistics are separated by site, parameter, year (WY or user defined), and sample method group.
+#' @details Statistics are separated by site, parameter, year (WY or user defined CY), and sample method group.
+#' @details User-defined calendar year. If \code{startMonth} and \code{endMonth} are specified, statistics and counts 
+#' will be limited to calendar year samples within the months specified. Ex. If \code{startMonth = 9} and \code{endMonth = 11},
+#' only samples from September to November will be included in summary stats and counts, and they will be grouped by calendar year.
 #' @details Sample method groups. Cross section (X-section) is defined as sample method 10 (EWI), 15 (EWT), or 20 (EDI).
 #' Point or non-cross section (Pt/non-X-section) is defined as sample method 30 (single vertical), 40 (multiple verticals), 
 #' 50 (point sample), 55 (composite - multiple point samples), 60 (weighted bottle), 70 (grab sample - dip), 100 (Van Dorn), 
@@ -29,7 +35,8 @@ calc_summaryStats <- function(x, pcodes = c("80154",
                                             "00530",
                                             "80155",
                                             "80225",
-                                            "91145"))
+                                            "91145"),
+                              startMonth = NULL, endMonth  = NULL)
 {
   # remove rejected samples
   x <- x[!(x$DQI %in% c("Q","X")),]
@@ -41,6 +48,19 @@ calc_summaryStats <- function(x, pcodes = c("80154",
   x$xsection[x$UID %in% xsection$UID] <- "X-Section"
   x$xsection[x$UID %in% nonxsection$UID] <- "Pt/non-X-section"
   x$xsection[is.na(x$xsection)] <- "not coded or other"
+  
+  #split up by user defined calendar year period, calendar year column heading renamed below
+  if(!is.null(startMonth) & !is.null(endMonth)){
+    x$month <- lubridate::month(x$SAMPLE_START_DT)
+    if(startMonth <= endMonth){
+      x <- x[x$month >= startMonth & x$month <= endMonth,]
+      x$WY[x$month >= 10] <- x$WY[x$month >=10] - 1
+    }
+    if(startMonth > endMonth){
+      stop("Start Month must be less than End Month")
+    }
+  }
+
   
   #limit data to pcodes of interest, no blanks, and no non-detect or averages, and no samples where no result value reported
   statSamples <- x[!(x$MEDIUM_CD == "OAQ") & x$PARM_CD %in% pcodes & !(x$REMARK_CD %in% c("<","A")) & !(is.na(x$RESULT_VA)), ]
@@ -55,6 +75,8 @@ calc_summaryStats <- function(x, pcodes = c("80154",
                            stdev = sd(RESULT_VA, na.rm = T))
   temp$mean <- format(temp$mean, scientific = FALSE)
   
+
+
   #count number of non-detect samples and join to output
   nonDet <- x[!(x$MEDIUM_CD == "OAQ") & x$PARM_CD %in% pcodes & x$REMARK_CD == "<", ]
   nonDet <- dplyr::summarise(dplyr::group_by(nonDet,SITE_NO,PARM_CD,WY,xsection),
@@ -68,6 +90,10 @@ calc_summaryStats <- function(x, pcodes = c("80154",
                              no_result_reported = length(RESULT_VA))
   temp <- dplyr::left_join(temp,naRes, by = c("SITE_NO"="SITE_NO","PARM_CD"="PARM_CD","WY"="WY","xsection"="xsection"))
   temp$no_result_reported[is.na(temp$no_result_reported)] <- 0
+  
+  if(!is.null(startMonth) & !is.null(endMonth)){
+    names(temp)[names(temp)=='WY'] <- 'CY'
+  }
   
   return(temp)
   
