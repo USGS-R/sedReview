9fe1e652cbdae5189ae37a93b77ba652ac7ef41c
Colin Penn <cpenn@usgs.gov>
2018-08-24 09:44:37 -0600

    modify plot_turbSSC to output single plot

--- tests/testthat/tests_plot_turbSSC.R
+++ tests/testthat/tests_plot_turbSSC.R
@@ -3,10 +3,10 @@ context('plot_turbSSC')
 test_that("Return values check", {
   data('exampleData2', package = "sedReview")
   
-  # plot_turbSSC <- plot_sedTS(exampleData2, siteSelect = "09163500")
+  # plot_turbSSC <- plot_turbSSC(exampleData2, siteSelect = "09163500")
   # saveRDS(plot_turbSSC, "tests/testthat/data/plot_turbSSC.rds")
   
   plot_turbSSC <- readRDS("data/plot_turbSSC.rds")
-  plot_turbSSCTest <- plot_sedTS(exampleData2, siteSelect = "09163500")
+  plot_turbSSCTest <- plot_turbSSC(exampleData2, siteSelect = "09163500")
   expect_equal(plot_turbSSC, plot_turbSSCTest)
 })
