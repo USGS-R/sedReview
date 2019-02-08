787f9eb70280033df762d195012622e674907cfc
Colin Penn <cpenn@usgs.gov>
2019-02-08 14:43:07 -0700

    add user manual to shiny GUI

--- inst/shiny/SedReviewGUI/app.R
+++ inst/shiny/SedReviewGUI/app.R
@@ -21,7 +21,7 @@ ui <- navbarPage(
            h3("Welcome to SedReview v1.0: Discrete sediment data review and exploration toolbox."),
            h4("Please use the 'Site-Level Assessment Module' to review and plot data from a specific site."),
            h4("Please use the 'Science-Center Review Module' to perform high-level data reviews for an entire Water Science Center."),
-           h4(helpText(a('User Guide', href=NULL,target="_blank"))),
+           h4(helpText(a('User Guide', href="sedReview_manual.html",target="_blank"))),
            h4(helpText(a("Additional Info: SedReview 1.0 GitHub",
                          href="https://github.com/USGS-R/sedReview",target="_blank")))
   ),
---
+++ inst/shiny/SedReviewGUI/www/build_rmarkdown.R
@@ -0,0 +1,3 @@
+rmarkdown::render('inst/shiny/SedReviewGUI/www/sedReview_manual.Rmd',
+                  output_format = 'html_document',
+                  output_file = 'sedReview_manual.html')
