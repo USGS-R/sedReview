2a14d4cdff47c7c96458bc35f1498a6b399d629d
Colin Penn <cpenn@usgs.gov>
2019-12-02 09:13:30 -0700

    make DFs useable in environment

--- inst/shiny/SedReviewGUI/server_SLA_1_dataImport.R
+++ inst/shiny/SedReviewGUI/server_SLA_1_dataImport.R
@@ -1,43 +1,51 @@
 #### Site-Level Assessment: Data import and import summary ####
 # Data Pull routine
-siteData <- eventReactive(input$dataPull, {
+observeEvent(input$dataPull, {
+  
+  siteData <<- NULL
+  importInfo <<- list()
+  sumStats <<- NULL
   
   # import data for your site using get_localNwis.
-  get_localNWIS(DSN = input$DBName,            # NWIS server 
-                env.db = input$env.db,          # environmental database number  
-                qa.db = input$qa.db,            # QA database number 
-                STAIDS = input$varSite,             
-                begin.date = input$beginDT, 
-                end.date = input$endDT)
+  siteData <<- get_localNWIS(DSN = input$DBName, # NWIS server 
+                             env.db = input$env.db,           # environmental database number  
+                             qa.db = input$qa.db,             # QA database number 
+                             STAIDS = input$varSite,             
+                             begin.date = input$beginDT, 
+                             end.date = input$endDT)
+  importInfo$DSN <<- input$DBName
+  importInfo$env.db <<- input$env.db          
+  importInfo$qa.db <<- input$qa.db
+  importInfo$STAIDS <<- input$varSite      
+  importInfo$begin.date <<- input$beginDT
+  importInfo$analysis.begin.date <<- input$analysisBeginDT
+  importInfo$end.date <<- input$endDT
+  
+  sumStats <<- calc_summaryStats(siteData)
+  output$sumtable <- DT::renderDataTable(
+    datatable({sumStats},
+              extensions = 'Buttons', 
+              rownames = FALSE,
+              options = list(dom = 'Bfrtip',
+                             buttons = 
+                               list('colvis', list(
+                                 extend = 'collection',
+                                 buttons = list(list(extend ='csv',
+                                                     filename = 'SiteSummaryTable'),
+                                                list(extend ='excel',
+                                                     filename = 'SiteSummaryTable'),
+                                                list(extend ='pdf',
+                                                     pageSize = 'A3',
+                                                     orientation = 'landscape',
+                                                     filename = 'SiteSummaryTable')),
+                                 text = 'Download'
+                               )),
+                             scrollX = TRUE,
+                             scrollY = "600px",
+                             order = list(list(4, 'desc'), list(2, 'asc')),
+                             pageLength = nrow({sumStats}),
+                             selection = 'single')
+              
+    ))
 })
 
-sumStats <- eventReactive(input$dataPull, {
-  sumStats <- calc_summaryStats(siteData())
-  return(sumStats)
-})
-
-output$sumtable <- DT::renderDataTable(
-  datatable({sumStats()},
-            extensions = 'Buttons', 
-            rownames = FALSE,
-            options = list(dom = 'Bfrtip',
-                           buttons = 
-                             list('colvis', list(
-                               extend = 'collection',
-                               buttons = list(list(extend ='csv',
-                                                   filename = 'SiteSummaryTable'),
-                                              list(extend ='excel',
-                                                   filename = 'SiteSummaryTable'),
-                                              list(extend ='pdf',
-                                                   pageSize = 'A3',
-                                                   orientation = 'landscape',
-                                                   filename = 'SiteSummaryTable')),
-                               text = 'Download'
-                             )),
-                           scrollX = TRUE,
-                           scrollY = "600px",
-                           order = list(list(4, 'desc'), list(2, 'asc')),
-                           pageLength = nrow({sumStats()}),
-                           selection = 'single')
-            
-  ))
--- inst/shiny/SedReviewGUI/server_SLA_2_rejected.R
+++ inst/shiny/SedReviewGUI/server_SLA_2_rejected.R
@@ -1,46 +1,46 @@
 #### Site-Level Assessment: Rejected data ####
-
 #Rejected data pull routine  
-rejectedData <- eventReactive(input$dataPull, {
+observeEvent(input$dataPull, {
   # import data for your site using get_localNwis.
-  rejDat <- get_localNWIS(DSN = input$DBName,            # NWIS server
-                          env.db = input$env.db,           # environmental database number
-                          qa.db = input$qa.db,            # QA database number
-                          STAIDS = input$varSite,
-                          begin.date = input$analysisBeginDT, # WY 2017
-                          end.date = input$endDT,
-                          approval = "Rejected")
-  rejDat <- rejDat[rejDat$PARM_CD %in% c("80154",
-                                         "70331",
-                                         "00530",
-                                         "80155",
-                                         "80225",
-                                         "91145"),]
-  return(rejDat)
+  rejectedData <<- NULL
+  rejectedData <<- get_localNWIS(DSN = input$DBName,            # NWIS server
+                                 env.db = input$env.db,           # environmental database number
+                                 qa.db = input$qa.db,            # QA database number
+                                 STAIDS = input$varSite,
+                                 begin.date = input$analysisBeginDT, # WY 2017
+                                 end.date = input$endDT,
+                                 approval = "Rejected")
+  rejectedData <- rejectedData[rejectedData$PARM_CD %in% c("80154",
+                                                           "70331",
+                                                           "00530",
+                                                           "80155",
+                                                           "80225",
+                                                           "91145"),]
+  output$rejectedtable <- DT::renderDataTable(
+    datatable({rejectedData[, -c(1, 3, 6:7, 11, 15, 17:35, 43:47, 49:52, 54:56, 58:65, 70:72, 74:79)]},
+              extensions = 'Buttons', 
+              rownames = FALSE,
+              options = list(dom = 'Bfrtip',
+                             buttons = 
+                               list('colvis', list(
+                                 extend = 'collection',
+                                 buttons = list(list(extend ='csv',
+                                                     filename = 'rejectedDataTable'),
+                                                list(extend ='excel',
+                                                     filename = 'rejectedDataTable'),
+                                                list(extend ='pdf',
+                                                     pageSize = 'A1',
+                                                     orientation = 'landscape',
+                                                     filename = 'rejectedDataTable')),
+                                 text = 'Download'
+                               )),
+                             scrollX = TRUE,
+                             scrollY = "600px",
+                             order = list(list(1, 'asc')),
+                             pageLength = nrow({rejectedData}),
+                             selection = 'single')
+              
+    ))
 })
 
-output$rejectedtable <- DT::renderDataTable(
-  datatable({rejectedData()[, -c(1, 3, 6:7, 11, 15, 17:35, 43:47, 49:52, 54:56, 58:65, 70:72, 74:79)]},
-            extensions = 'Buttons', 
-            rownames = FALSE,
-            options = list(dom = 'Bfrtip',
-                           buttons = 
-                             list('colvis', list(
-                               extend = 'collection',
-                               buttons = list(list(extend ='csv',
-                                                   filename = 'rejectedDataTable'),
-                                              list(extend ='excel',
-                                                   filename = 'rejectedDataTable'),
-                                              list(extend ='pdf',
-                                                   pageSize = 'A1',
-                                                   orientation = 'landscape',
-                                                   filename = 'rejectedDataTable')),
-                               text = 'Download'
-                             )),
-                           scrollX = TRUE,
-                           scrollY = "600px",
-                           order = list(list(1, 'asc')),
-                           pageLength = nrow({rejectedData()}),
-                           selection = 'single')
-            
-  ))
+
